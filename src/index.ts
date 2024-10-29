66b4d53a5fa167f3eca6b82e04c2a3091440c061
Yury Ahanouski <uraaru@live.com>
2024-10-29 22:43:44 +0300

    feat: add husky pre-commite eslint check, cors and helmet express security

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,20 @@ import { productsRouter } from '@/routes/ecommerce/products.route';
 import { userRouter } from '@/routes/ecommerce/user.route';
 import chalk from 'chalk';
 import express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+
+const corsOptions = {
+  origin: 'http://localhost:5173/',
+  credentials: true
+};
 
 function start() {
   const PORT = process.env.PORT ?? 5000;
   const app = express();
 
+  app.use(cors(corsOptions));
+  app.use(helmet());
   app.use(morganChalk);
   app.use(express.json());
 
