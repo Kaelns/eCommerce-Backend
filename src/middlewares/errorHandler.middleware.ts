b6c467a82846a1995665dfc1d38b651c57034096
Yury Ahanouski <uraaru@live.com>
2024-10-29 19:53:07 +0300

    feat: add zod middleware input validator

--- src/middlewares/errorHandler.middleware.ts
+++ src/middlewares/errorHandler.middleware.ts
@@ -4,13 +4,8 @@ import type { NextFunction, Request, Response } from 'express';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function errorHandler(err: Error, _req: Request, res: Response, _next: NextFunction) {
   res.status(res.statusCode !== 200 ? res.statusCode : 500);
+  err.stack = process.env.NODE_ENV === 'production' ? 'Not development' : err.stack;
 
-  const responceBody = {
-    name: err.name,
-    message: err.message,
-    stack: process.env.NODE_ENV === 'production' ? 'Not development' : err.stack
-  };
-
-  console.log(chalk.red('Error'), '\n', responceBody);
-  res.json(responceBody);
+  console.log(chalk.red('Error'), '\n', err);
+  res.json(err);
 }
--- src/routes/base.route.ts
+++ src/routes/base.route.ts
@@ -1,6 +1,18 @@
+import { zodValidate } from '@/middlewares/zodValidate.middleware';
 import express from 'express';
+import { z } from 'zod';
 
 const baseRouter = express.Router();
+
+const ApiScheme = z.object({
+  id: z.number(),
+  name: z.string(),
+  email: z.string().email().min(5),
+  data: z.object({
+    shippingAddress: z.string(),
+    billingAddress: z.string()
+  })
+});
 
 baseRouter
   .route('/')
@@ -8,7 +20,7 @@ baseRouter
     const message = `Server is ongoing on ${process.env.PORT} with ${process.env.PROJECT_KEY} on mode ${process.env.NODE_ENV}`;
     res.status(200).send({ message });
   })
-  .post((req, res) => {
+  .post(zodValidate(ApiScheme), (req, res) => {
     console.log(req.body);
     const message = `Server is ongoing on ${process.env.PORT}`;
     res.status(200).send({ message });
